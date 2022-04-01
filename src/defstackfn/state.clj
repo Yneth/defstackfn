4bfded4a445468d0389a01daf957dd2b140b51bc
Anthony Bondarenko <anton.bondarenko.ua@gmail.com>
2022-04-01 20:13:12 +0300

    cleanup unnecessary typehints

--- src/defstackfn/state.clj
+++ src/defstackfn/state.clj
@@ -9,14 +9,14 @@
 (defn init-state [vars]
   (State. ::nil vars))
 
-(defn ^State stack-push [^State state val]
+(defn stack-push [state val]
   (update state :stack
     (fn [stack]
       (if (= ::nil stack)
         (list val)
         (conj stack val)))))
 
-(defn ^State stack-pop [^State state]
+(defn stack-pop [state]
   (update state :stack
     (fn [stack]
       (cond
@@ -29,31 +29,31 @@
         :else
         (rest stack)))))
 
-(defn ^State stack-pop-multi [^State state n]
+(defn stack-pop-multi [state n]
   (reduce
     (fn [acc _]
       (stack-pop acc))
     state
     (range n)))
 
-(defn ^IPersistentList stack-take [^State state n]
+(defn ^IPersistentList stack-take [state n]
   (let [stack (get state :stack)]
     (if (= ::nil stack)
       (list)
       (take n stack))))
 
-(defn ^Object get-stack-head [^State state]
+(defn ^Object get-stack-head [state]
   (let [stack (get state :stack)]
     (if (= ::nil stack)
       stack
       (first stack))))
 
-(defn stack-empty? [^State state]
+(defn stack-empty? [state]
   (= ::nil (get state :stack)))
 
-(defn ^Object get-var [^State state ^Symbol var-name or-else]
+(defn ^Object get-var [state ^Symbol var-name or-else]
   (get-in state [:vars var-name] or-else))
 
-(defn ^State define-var [^State state ^Symbol var-name]
+(defn define-var [state ^Symbol var-name]
   (let [var-value (get-stack-head state)]
     (update state :vars assoc var-name var-value)))
